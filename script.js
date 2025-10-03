7d8e472d8c60630a997f161fc35b21a6afcbd250
anna_b <nelson_anna9@hotmail.com>
2025-10-03 12:21:10 -0500

    Add change color and clear features

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 const gridContainer = document.querySelector(".grid-container");
 const pixelButton = document.querySelector(".pixel-button");
+const clearButton = document.querySelector(".clear-button");
+const colorPicker = document.querySelector("#color-picker");
+let marker = colorPicker.value;
 
+// Display an initial 16 x 16 grid
 for (i=1; i<=256; i++){
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("square");
     gridContainer.append(gridSquare);
-    gridSquare.addEventListener("mouseover", function(e) {
-        e.target.style.backgroundColor = "#365571";
+    gridSquare.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = marker;
     });
 };
 
-/*gridSquare.addEventListener("mouseover", function(e) {
-    e.target.style.backgroundColor = "#365571";
-});*/
+// Change Marker Color Button
+colorPicker.addEventListener("input", (e) => {
+    marker = e.target.value;
+});
 
-pixelButton.addEventListener("click", function() {
+// Clear Button - must wait for initial DOM to load before we can select the squares
+document.addEventListener("DOMContentLoaded", () => {
+    clearButton.addEventListener("click", () => {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(square => square.style.backgroundColor = "#cedfd9")
+    });
+});
+
+// Change Pixelation Button
+pixelButton.addEventListener("click", () => {
     let gridSize = Number(prompt("How many squares per side would you like your grid to have? Must be a number 1 - 100"));
     makeAGrid(gridSize);
 });
 
 function makeAGrid(gridSize){
-    gridContainer.innerHTML = "";
     if (gridSize > 100 || gridSize < 1) {
         alert("Please enter a number between 1 - 100.");
     } else if (isNaN(gridSize)) {
         alert("Please enter a number between 1 - 100.");
     } else {
+        gridContainer.innerHTML = "";
         let gridCondition = gridSize * gridSize;
         let squareWidth = ((1 / gridSize) * 100).toFixed(2) + "%";
         let squarePadBot = squareWidth;
@@ -35,12 +49,10 @@ function makeAGrid(gridSize){
             gridSquare.style.width = squareWidth;
             gridSquare.style.paddingBottom = squarePadBot;
             gridContainer.append(gridSquare);
-            gridSquare.addEventListener("mouseover", function(e) {
-                e.target.style.backgroundColor = "#365571";
+            gridSquare.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = marker;
             });
         };
-    
-    
     };
 };
 
